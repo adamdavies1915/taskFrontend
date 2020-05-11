23679ccafae95f80804831c42c13638f38294dd4
Adam Davies <adam.davies@pepperhq.com>
2020-05-11 16:11:54 +0100

    added a text input to filter table by artist using new fuzzy search

--- src/api/trackApi.ts
+++ src/api/trackApi.ts
@@ -1,10 +1,13 @@
-import axios from 'axios'
-import { Track } from '../types/Track';
+import axios from "axios";
+import { Track } from "../types/Track";
 
-export async function fetchAllTracks() : Promise<Track[]> {
-    
-  const response = await axios(
-      'http://localhost:3000/track/',
-    );
-    return response.data;
+export async function fetchAllTracks(): Promise<Track[]> {
+  const response = await axios("http://localhost:3000/track/");
+  return response.data;
+}
+
+export async function fetchTracksForArtist(artist: string): Promise<Track[]> {
+  const response = await axios(`http://localhost:3000/track?artist=${artist}`);
+  return response.data;
+
 }
--- src/components/TracksTable.tsx
+++ src/components/TracksTable.tsx
@@ -1,39 +1,58 @@
-import React, { useState, useEffect } from "react";
-import { Table } from "reactstrap";
+import React, { useState, useEffect, ChangeEvent} from "react";
+import { Table, Form, FormGroup, Label, Input } from "reactstrap";
 import { Track } from "../types/Track";
-import { fetchAllTracks } from "../api/trackApi";
+import { fetchAllTracks, fetchTracksForArtist } from "../api/trackApi";
 
 export function TracksTable() {
-    const [tracks, setTracks] = useState([] as Track[]);
-    
-    useEffect(() => {
-        async function fetchData() {
-          const tracks = await fetchAllTracks();
-          setTracks(tracks);
-        }
-        fetchData();
-      }, []);
+  const [tracks, setTracks] = useState<Array<Track>>([]);
 
-    return (
-        <div>
-            <Table>
-                <thead>
-                    <tr>
-                    <th>ID</th>
-                    <th>Artist</th>
-                    <th>Title</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {tracks.map((track, index) => {
-                    return <tr key={index}>
-                        <td>{track.id}</td>
-                        <td>{track.artist}</td>
-                        <td>{track.title}</td>
-                        </tr>
-                })}
-                </tbody>
-            </Table>
-        </div>
-    );
+  useEffect(() => {
+    async function fetchData() {
+      const tracks = await fetchAllTracks();
+      setTracks(tracks);
+    }
+    fetchData();
+  }, []);
+
+  const onArtistFieldChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const tracks = await fetchTracksForArtist(event.target.value);
+    setTracks(tracks);
+  }
+
+  return (
+    <div>
+      <Form>
+        <FormGroup>
+          <Label for="artist">Artist</Label>
+          <Input
+            type="text"
+            name="artist"
+            id="artistFilter"
+            placeholder="Filter by artist"
+            onChange={onArtistFieldChange}
+          />
+        </FormGroup>
+      </Form>
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Artist</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tracks.map((track) => {
+            return (
+              <tr key={track.id}>
+                <td>{track.id}</td>
+                <td>{track.artist}</td>
+                <td>{track.title}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
